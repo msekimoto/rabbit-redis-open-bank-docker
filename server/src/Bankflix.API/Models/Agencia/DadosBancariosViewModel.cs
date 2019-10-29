28c5e761243b2bd86034d16a4472ac2a4951d059
Marcelo Sekimoto <msekimoto@gmail.com>
2019-10-28 23:42:30 -0300

    init

---
+++ server/src/Bankflix.API/Models/Agencia/DadosBancariosViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Bankflix.API.Models.Agencia
+{
+    public class DadosBancariosViewModel
+    {
+        public string NumeroAgencia { get; set; }
+        public string DigitoVerificador { get; set; }
+    }
+}
---
+++ server/src/Movimentacoes.Domain/Contas/Repository/IContaRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Movimentacoes.Domain.Contas.Repository
+{
+    public interface IContaRepository
+    {
+        Conta ObterPorNumeroComDigitoVerificador(string numero, string digitoVerificador);
+        Conta ObterPorId(Guid id);
+        Conta ObterPorCliente(Guid clienteId);
+    }
+}
